0fa7e80bc80e8c011b9a28803afddeb7c93ada8b
aleks100s <aleksandr100s@yandex.ru>
2026-05-23 11:42:29 +0700

    Present Game Center VC if needed

--- iosApp/iosApp/Helpers/IOSAccountService.swift
+++ iosApp/iosApp/Helpers/IOSAccountService.swift
@@ -7,12 +7,15 @@
 
 import ComposeApp
 import GameKit
+import UIKit
 
 final class IOSAccountService: MutableNativeAccountService {
     override init() {
         super.init()
-        GKLocalPlayer.local.authenticateHandler = { viewController, error in
-            self.handleAuthentication(viewController: viewController, error: error)
+        GKLocalPlayer.local.authenticateHandler = { [weak self] viewController, error in
+            DispatchQueue.main.async {
+                self?.handleAuthentication(viewController: viewController, error: error)
+            }
         }
     }
     
@@ -20,20 +23,61 @@ final class IOSAccountService: MutableNativeAccountService {
         viewController: UIViewController?,
         error: Error?
     ) {
-        let hideMultiplayer = GKLocalPlayer.local.isUnderage ||
-            GKLocalPlayer.local.isMultiplayerGamingRestricted
-        updateHideMultiplayer(hideMultiplayer: hideMultiplayer)
-        updateIsAuthorized(isAuthorized: GKLocalPlayer.local.isAuthenticated)
-        updateAuthorizedUserName(name: GKLocalPlayer.local.displayName)
-
         if let viewController {
-            UIApplication.shared.delegate?.window??.rootViewController?.present(viewController, animated: true)
+            present(viewController: viewController)
             return
         }
         
         if let error {
             print(error.localizedDescription)
+            updateIsAuthorized(isAuthorized: false)
             return
         }
+        
+        let hideMultiplayer = GKLocalPlayer.local.isUnderage ||
+            GKLocalPlayer.local.isMultiplayerGamingRestricted
+        updateHideMultiplayer(hideMultiplayer: hideMultiplayer)
+        updateIsAuthorized(isAuthorized: GKLocalPlayer.local.isAuthenticated)
+        updateAuthorizedUserName(name: GKLocalPlayer.local.displayName)
+    }
+    
+    private func present(viewController: UIViewController) {
+        guard let presenter = UIApplication.shared.activeTopViewController else {
+            return
+        }
+        
+        presenter.present(viewController, animated: true)
+    }
+}
+
+private extension UIApplication {
+    var activeTopViewController: UIViewController? {
+        connectedScenes
+            .compactMap { $0 as? UIWindowScene }
+            .first { $0.activationState == .foregroundActive }?
+            .windows
+            .first { $0.isKeyWindow }?
+            .rootViewController?
+            .topViewController
+    }
+}
+
+private extension UIViewController {
+    var topViewController: UIViewController {
+        if let presentedViewController {
+            return presentedViewController.topViewController
+        }
+        
+        if let navigationController = self as? UINavigationController,
+           let visibleViewController = navigationController.visibleViewController {
+            return visibleViewController.topViewController
+        }
+        
+        if let tabBarController = self as? UITabBarController,
+           let selectedViewController = tabBarController.selectedViewController {
+            return selectedViewController.topViewController
+        }
+        
+        return self
     }
 }
